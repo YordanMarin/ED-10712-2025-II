ddf299adc52a39458d1e46d671c583043cfe8973
yordan <yordan@gmail.com>
2025-09-05 12:15:51 -0500

    lista doble y circular

---
+++ Lista enlazada doble/Lista.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lista_enlazada_doble
+{
+  internal class Lista
+  {
+    Nodo primero = null;
+    Nodo ultimo = null;
+
+    public void insertar(int num)
+    {
+      Nodo nuevo = new Nodo();
+      nuevo.Numero = num;
+
+      if(primero == null)
+      {
+        primero = nuevo;
+        nuevo.Siguiente = null;
+        nuevo.Anterior = null;
+        ultimo = nuevo;
+      }
+      else
+      {
+        ultimo.Siguiente = nuevo;
+        nuevo.Siguiente = null;
+        nuevo.Anterior = ultimo;
+        ultimo = nuevo;
+      }
+    }
+
+    public void mostrar(ListBox list)
+    {
+      Nodo actual = primero;
+
+      while( actual != null)
+      {
+        list.Items.Add(actual.Numero);
+        actual = actual.Siguiente;
+      }
+    }
+
+    public void eliminar(int num)
+    {
+      Nodo actual = primero;
+      while( actual != null)
+      {
+        if(actual.Numero == num){
+
+        }
+        actual = actual.Siguiente;
+      }
+    }
+  }
+}
