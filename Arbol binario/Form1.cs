f2e186ae8c0c61df2edcdcd140de6e87ab5ba942
yordan <yordan@gmail.com>
2025-10-10 12:06:15 -0500

    sem8

---
+++ Arbol binario/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Arbol_binario
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+      radioIzquierda.Checked = true;
+    }
+    Nodo raiz;
+    Nodo sele;
+    arbol a = new arbol();
+    private void btnInsertarRaiz_Click(object sender, EventArgs e)
+    {
+      treeView1.Nodes.Clear();
+      raiz = a.insertar(null, int.Parse(textNumero.Text));
+      a.mostrar(raiz, treeView1, null);
+    }
+
+    private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+    {
+      sele = (Nodo)e.Node.Tag;
+    }
+
+    private void btnInsertarRama_Click(object sender, EventArgs e)
+    {
+      if (sele != null)
+      {
+        if (radioIzquierda.Checked == true)
+          sele.izquierda = a.insertar(sele.izquierda, int.Parse(textNumero.Text));
+        if (radioDerecha.Checked == true)
+          sele.derecha = a.insertar(sele.derecha, int.Parse(textNumero.Text));
+
+        treeView1.Nodes.Clear();
+        a.mostrar(raiz, treeView1, null);
+        treeView1.ExpandAll();
+        textNumero.Clear();
+      }
+      else MessageBox.Show("Seleccione un nodo.");
+      
+    }
+
+    private void btnAltura_Click(object sender, EventArgs e)
+    {
+      MessageBox.Show("Altura: " + a.altura(raiz));
+    }
+  }
+}
---
+++ Arbol binario/arbol.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Arbol_binario
+{
+  internal class arbol
+  {
+    public Nodo insertar(Nodo actual, int n)
+    {
+      Nodo nuevo = new Nodo(n);
+
+      if (actual == null) return nuevo;
+      if ((actual.izquierda != null || actual.derecha == null) &&
+        (actual.izquierda == null || actual.derecha != null))
+      {
+        MessageBox.Show("Nodo ocupado");
+      }
+      else if (actual.izquierda == null)
+        actual.izquierda = nuevo;
+      else actual.derecha = nuevo;
+
+      return actual;
+    }
+
+    public void mostrar(Nodo actual, TreeView tree, TreeNode tallo)
+    {
+      TreeNode raiz = new TreeNode(actual.numero.ToString()); //raiz
+      raiz.Tag = actual; //almacenando referencia de memoria
+
+      if(tallo == null) tree.Nodes.Add(raiz); //tallo
+      else tallo.Nodes.Add(raiz); //hojas
+
+      if(actual.izquierda != null) mostrar(actual.izquierda, tree, raiz);
+      if (actual.derecha != null) mostrar(actual.derecha, tree, raiz);
+    }
+
+    public int altura(Nodo actual)
+    {
+      if(actual != null)
+      {
+        int altIzq = altura(actual.izquierda);
+        int altDer = altura(actual.derecha);
+
+        return Math.Max(altIzq, altDer)+1;
+      }
+      return 0;
+    }
+  }
+}
