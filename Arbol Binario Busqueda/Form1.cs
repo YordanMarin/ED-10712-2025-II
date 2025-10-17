df7df472039b9db719d209c7ebfb8750ba06de91
yordan <yordan@gmail.com>
2025-10-17 12:13:00 -0500

    sem9

---
+++ Arbol Binario Busqueda/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Arbol_Binario_Busqueda
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void btnInsertar_Click(object sender, EventArgs e)
+    {
+      try
+      {
+        //copia y pega todo
+      }
+      catch (Exception)
+      {
+        MessageBox.Show("Solo se permiten numeros");
+      }
+    }
+  }
+}
