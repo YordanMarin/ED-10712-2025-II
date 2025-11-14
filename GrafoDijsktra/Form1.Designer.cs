e4a4ae42d26877f959c0cf4a525a106013bc5d74
Yordan <yordan@gmail.com>
2025-11-14 12:07:08 -0500

    sem13

---
+++ GrafoDijsktra/Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace GrafoDijsktra
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnVertices = new System.Windows.Forms.Button();
+            this.textVert = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textCosto = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textDestino = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnInsertar = new System.Windows.Forms.Button();
+            this.textOrigen = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnDijsktra = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnMatriz = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(396, 149);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(410, 37);
+            this.label5.TabIndex = 65;
+            this.label5.Text = "Ingrese número de vertices:";
+            // 
+            // btnVertices
+            // 
+            this.btnVertices.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVertices.Location = new System.Drawing.Point(984, 132);
+            this.btnVertices.Name = "btnVertices";
+            this.btnVertices.Size = new System.Drawing.Size(327, 61);
+            this.btnVertices.TabIndex = 66;
+            this.btnVertices.Text = "Insertar";
+            this.btnVertices.UseVisualStyleBackColor = true;
+            this.btnVertices.Click += new System.EventHandler(this.btnVertices_Click);
+            // 
+            // textVert
+            // 
+            this.textVert.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textVert.Location = new System.Drawing.Point(806, 141);
+            this.textVert.Name = "textVert";
+            this.textVert.Size = new System.Drawing.Size(146, 44);
+            this.textVert.TabIndex = 67;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textCosto);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.textDestino);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.btnInsertar);
+            this.groupBox1.Controls.Add(this.textOrigen);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(62, 111);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Size = new System.Drawing.Size(315, 325);
+            this.groupBox1.TabIndex = 70;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Aristas";
+            // 
+            // textCosto
+            // 
+            this.textCosto.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textCosto.Location = new System.Drawing.Point(148, 164);
+            this.textCosto.Name = "textCosto";
+            this.textCosto.Size = new System.Drawing.Size(109, 44);
+            this.textCosto.TabIndex = 47;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(14, 167);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(110, 37);
+            this.label4.TabIndex = 46;
+            this.label4.Text = "Costo:";
+            // 
+            // textDestino
+            // 
+            this.textDestino.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textDestino.Location = new System.Drawing.Point(148, 100);
+            this.textDestino.Name = "textDestino";
+            this.textDestino.Size = new System.Drawing.Size(112, 44);
+            this.textDestino.TabIndex = 42;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(16, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(134, 37);
+            this.label3.TabIndex = 41;
+            this.label3.Text = "Destino:";
+            // 
+            // btnInsertar
+            // 
+            this.btnInsertar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInsertar.Location = new System.Drawing.Point(86, 225);
+            this.btnInsertar.Name = "btnInsertar";
+            this.btnInsertar.Size = new System.Drawing.Size(177, 75);
+            this.btnInsertar.TabIndex = 40;
+            this.btnInsertar.Text = "Insertar";
+            this.btnInsertar.UseVisualStyleBackColor = true;
+            this.btnInsertar.Click += new System.EventHandler(this.btnInsertar_Click);
+            // 
+            // textOrigen
+            // 
+            this.textOrigen.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textOrigen.Location = new System.Drawing.Point(148, 38);
+            this.textOrigen.Name = "textOrigen";
+            this.textOrigen.Size = new System.Drawing.Size(109, 44);
+            this.textOrigen.TabIndex = 39;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(16, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 37);
+            this.label2.TabIndex = 38;
+            this.label2.Text = "Origen:";
+            // 
+            // btnDijsktra
+            // 
+            this.btnDijsktra.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDijsktra.Location = new System.Drawing.Point(395, 641);
+            this.btnDijsktra.Name = "btnDijsktra";
+            this.btnDijsktra.Size = new System.Drawing.Size(916, 75);
+            this.btnDijsktra.TabIndex = 69;
+            this.btnDijsktra.Text = "Ruta mínima DIJSKTRA";
+            this.btnDijsktra.UseVisualStyleBackColor = true;
+            this.btnDijsktra.Click += new System.EventHandler(this.btnDijsktra_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 37;
+            this.listBox2.Location = new System.Drawing.Point(395, 741);
+            this.listBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(914, 263);
+            this.listBox2.TabIndex = 68;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(395, 304);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(914, 327);
+            this.richTextBox1.TabIndex = 64;
+            this.richTextBox1.Text = "";
+            // 
+            // btnMatriz
+            // 
+            this.btnMatriz.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMatriz.Location = new System.Drawing.Point(395, 213);
+            this.btnMatriz.Name = "btnMatriz";
+            this.btnMatriz.Size = new System.Drawing.Size(916, 75);
+            this.btnMatriz.TabIndex = 63;
+            this.btnMatriz.Text = "Matriz de Costo";
+            this.btnMatriz.UseVisualStyleBackColor = true;
+            this.btnMatriz.Click += new System.EventHandler(this.btnMatriz_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 37;
+            this.listBox1.Location = new System.Drawing.Point(128, 486);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(217, 522);
+            this.listBox1.TabIndex = 62;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(371, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(618, 67);
+            this.label1.TabIndex = 61;
+            this.label1.Text = "GRAFOS DIRIGIDOS";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1419, 1079);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnVertices);
+            this.Controls.Add(this.textVert);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnDijsktra);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnMatriz);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnVertices;
+        private System.Windows.Forms.TextBox textVert;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textCosto;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textDestino;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnInsertar;
+        private System.Windows.Forms.TextBox textOrigen;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnDijsktra;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnMatriz;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ GrafoDijsktra/Grafo.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GrafoDijsktra
+{
+    internal class Grafo
+    {
+        int[,] matriz;
+        int vertices;
+
+        public Grafo(int vertices)
+        {
+            this.vertices = vertices;
+            matriz = new int[vertices, vertices];
+        }
+
+        public void imprimirmatriz(RichTextBox rich)
+        {
+            for (int i = 0; i < vertices; i++)
+            {
+                for (int j = 0; j < vertices; j++)
+                {
+                    rich.Text += matriz[i, j] + "\t";
+                }
+                rich.Text += "\n";
+            }
+        }
+
+        public void insertarArista(int o, int d, int c)
+        {
+            matriz[o, d] = c;
+        }
+
+        public void imprimirArista(ListBox list, int o, int d, int c)
+        {
+            list.Items.Add($"{o} | {d} | {c}");
+        }
+
+        public void dijsktra(ListBox list)
+        {
+            int inicio = 0;
+            int[] distancias = new int[vertices];
+            bool[] visitados = new bool[vertices];
+
+            for(int i = 0; i < vertices; i++)
+            {
+                distancias[i] = int.MaxValue;
+                visitados[i] = false;
+            }
+            distancias[inicio] = 0;
+
+            for(int i = 0; i < vertices-1; i++)
+            {
+                int actual = minDistancia(distancias, visitados);
+                visitados[actual] = true;
+
+                for(int j = 0; j < vertices; j++)
+                {
+                    if (!visitados[j] && matriz[actual, j] != 0 && distancias[i] != int.MaxValue &&
+                        distancias[actual] + matriz[actual,j] < distancias[j])
+                    {
+                        distancias[j] = distancias[actual] + matriz[actual, j];
+                    }
+                }
+            }
+
+            list.Items.Add("Origen\tDestino\tRuta mínima");
+            for(int i = 0; i < vertices; i++)
+            {
+                if (distancias[i] == int.MaxValue)
+                    list.Items.Add($"{inicio}\t{i}\tIdeterminado");
+                else list.Items.Add($"{inicio}\t{i}\t{distancias[i]}");
+            }
+        }
+
+        public int minDistancia(int[] distancias, bool[] visitados)
+        {
+            int min = int.MaxValue;
+            int index = -1;
+
+            for( int i = 0;i < distancias.Length; i++)
+            {
+                if (!visitados[i] & distancias[i] <= min)
+                {
+                    min = distancias[i];
+                    index = i;
+                }
+            }
+            return index;
+        }
+    }
+}
