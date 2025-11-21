66a75cdbd37c7e1a0be7aaff049665362a27ed62
yordan <yordan@gmail.com>
2025-11-21 11:46:34 -0500

    sem14

---
+++ GrafoFloyd/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GrafoFloyd
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+    Grafo g;
+    private void btnVertices_Click(object sender, EventArgs e)
+    {
+      g = new Grafo(int.Parse(textVert.Text));
+      listBox1.Items.Clear();
+      richTextBox2.Clear();
+      richTextBox1.Clear();
+    }
+
+    private void btnInsertar_Click(object sender, EventArgs e)
+    {
+      int o = int.Parse(textOrigen.Text);
+      int d = int.Parse(textDestino.Text);
+      int c = int.Parse(textCosto.Text);
+
+      g.insertarArista(o, d, c);
+      g.imprimirArista(listBox1, o, d, c);
+    }
+
+    private void btnMatriz_Click(object sender, EventArgs e)
+    {
+      if (g != null)
+      {
+        richTextBox1.Clear();
+        g.imprimirmatriz(richTextBox1);
+      }
+      else MessageBox.Show("Primero define la cantidad de vertices.");
+    }
+
+    private void btnDijsktra_Click(object sender, EventArgs e)
+    {
+      if (g != null)
+      {
+        richTextBox2.Clear();
+        g.floydWarshall(richTextBox2);
+      }
+      else MessageBox.Show("Primero define la cantidad de vertices.");
+    }
+  }
+}
---
+++ GrafoFloyd/Grafo.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GrafoFloyd
+{
+  internal class Grafo
+  {
+    int[,] matriz;
+    int vertices;
+
+    public Grafo(int vertices)
+    {
+      this.vertices = vertices;
+      matriz = new int[vertices, vertices];
+    }
+
+    public void imprimirmatriz(RichTextBox rich)
+    {
+      for (int i = 0; i < vertices; i++)
+      {
+        for (int j = 0; j < vertices; j++)
+        {
+          rich.Text += matriz[i, j] + "\t";
+        }
+        rich.Text += "\n";
+      }
+    }
+
+    public void insertarArista(int o, int d, int c)
+    {
+      matriz[o, d] = c;
+    }
+
+    public void imprimirArista(ListBox list, int o, int d, int c)
+    {
+      list.Items.Add($"{o} | {d} | {c}");
+    }
+
+    public void floydWarshall(RichTextBox rich)
+    {
+      //1.copiar la matrix de costo reemplazando 0 por infinito
+      int[,] floyd = new int[vertices, vertices];
+      for (int i = 0;i < vertices; i++)
+      {
+        for(int j = 0;j < vertices; j++)
+        {
+          if(matriz[i, j] != 0)
+            floyd[i, j] = matriz[i, j];
+          else 
+            floyd[i, j] = int.MaxValue;
+        }
+      }
+
+      //2. Algoritmo de floyd
+      for(int k = 0; k < vertices; k++)
+      {
+        for(int i =0; i < vertices; i++)
+        {
+          for (int j = 0; j < vertices; j++)
+          {
+            if (floyd[i,k]!=int.MaxValue && floyd[k,j]!= int.MaxValue)
+            {
+              int nuevoCosto = floyd[i, k] + floyd[k, j];
+              if (nuevoCosto < floyd[i,j])
+              {
+                floyd[i, j]= nuevoCosto;
+              }
+            }
+          }
+        }
+      }
+
+      //3. Imprimir resultados
+      for (int i = 0; i < vertices; i++)
+      {
+        for (int j = 0; j < vertices; j++)
+        {
+          if (i==j)
+            rich.Text += "0\t";
+          else if (floyd[i, j] == int.MaxValue)
+            rich.Text += "∞\t";
+          else
+            rich.Text += floyd[i, j] + "\t";
+        }
+        rich.Text += "\n";
+      }
+    }
+  }
+}
